6e1604b8f28899880a8ee86b327c492ece829ebf
Okhaimie <okhaimie.dev@gmail.com>
2022-02-13 21:08:20 -0600

    Uncommented proposal route

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHashHistory } from "vue-router"
 import Voting from "@/components/sections/Voting.vue"
-// import Proposal from "@/components/sections/Proposal.vue"
-// import NewProposal from "@/components/sections/NewProposal.vue"
+import Proposal from "@/components/sections/Proposal.vue"
+import NewProposal from "@/components/sections/NewProposal.vue"
 
 export default new createRouter({
   history: createWebHashHistory(),
@@ -16,17 +16,17 @@ export default new createRouter({
       props: true,
       component: Voting,
     },
-    // {
-    //   path: "/dao/:assetId/proposals/:proposalId",
-    //   name: "proposal",
-    //   props: true,
-    //   component: Proposal,
-    // },
-    // {
-    //   path: "/dao/:assetId/proposals/create",
-    //   name: "newProposal",
-    //   props: true,
-    //   component: NewProposal,
-    // },
+    {
+      path: "/dao/:assetId/proposals/:proposalId",
+      name: "proposal",
+      props: true,
+      component: Proposal,
+    },
+    {
+      path: "/dao/:assetId/proposals/create",
+      name: "newProposal",
+      props: true,
+      component: NewProposal,
+    },
   ],
 })

f0eeff558699a1b63cd5bdaf9b1f86a74e221195
immutablegoblin <bill0x2a@protonmail.com>
2022-10-12 18:58:02 +0100

    New connect wallet

--- src/components/sections/WalletConnect.vue
+++ src/components/sections/WalletConnect.vue
@@ -1,6 +1,9 @@
 <template>
 <div>
-    <div class="cointainer p-5">
+    <div class="p-5">
+        <div class="title-box">
+          <h3>Connect Wallet</h3>
+        </div>
         <div class="close-icon" @click="routeToHome">
              <unicon
                 class="is-flex is-align-self-center" 
@@ -10,13 +13,22 @@
             >
             </unicon>
         </div>
-        <div class="is-flex is-justify-content-space-evenly">
-            <div :class='["wallet-box", "m-0", "p-0", wallet.disabled ? "disabled" : "is-clickable"]' v-for="wallet in wallets" :key="wallet.name" @click="!wallet.disabled ? onClick(wallet.name) : ''" :aria-disabled="wallet.disabled">
-                <div class="image m-0 p-5 has-radius-md" >
-                    <img :src="wallet.icon"/>
+        <div class="is-flex is-justify-content-center">
+          <div class="is-inline-flex is-justify-content-space-evenly is-flex-direction-column">
+              <div 
+                :class='["wallet-box", "my-2", "p-0", wallet.disabled ? "disabled" : "is-clickable"]'
+                v-for="wallet in wallets"
+                :key="wallet.name"
+                @click="!wallet.disabled ? onClick(wallet.name) : ''"
+                :aria-disabled="wallet.disabled">
+                <div class="wallet-connector" >
+                    <img class="image is-64x64 mr-2 ml-5" :src="wallet.icon"/>
+                    <h4>{{ wallet.name.charAt(0).toUpperCase() + wallet.name.slice(1) }}</h4>
                 </div>
-            </div>
-        </div>
+              </div>
+              <p class="warning-message">Please ensure you are connected to Arbitrum One</p>
+          </div>
+      </div>
     </div>
 </div>    
 </template>
@@ -71,9 +83,6 @@ export default {
 <style lang="scss" scoped>
 @import "@/styles/frabric-custom.scss";
 
-.wallet-box :hover {
-  background-color: $mediumDarkGray;
-}
 
 .disabled {
   opacity: 0.4;
@@ -98,5 +107,60 @@ export default {
     background: $red;
     
   }
+}
+
+.title-box {
+  padding: 10px;
+  border-top-left-radius: 8px;
+  background: $mediumBlue;
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  h3 {
+    color: white;
+    font-weight: 400;
+  }
+}
+
+.wallet-connector {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 5px 0;
+  padding: 10px 20px 10px 0;
+  background: rgba(255,255,255, 0.03);
+  transition: all 150ms linear;
+  border-radius: 8px;
+  max-width: min(350px, 100%);
+  border-left: 8px solid $mediumBlue;
+  gap: 4rem;
+
+  @media screen and (max-width: 415px) {
+    flex-direction: column;
+    padding-right: 0px;
+    gap: 10px;
+
+    .image {
+      margin-left: 0px !important;
+    }
+  }
+
+  &:hover {
+    background-color: $mediumDarkGray;
+    transform: translateY(-3px);
+  }
+
+  h4 {
+    color: white;
+    font-weight: 400;
+    font-size: 1.4rem;
+    width: 8ch;
+  }
+}
+
+.warning-message {
+  max-width: 32ch;
+  text-align: center;
 }
 </style>

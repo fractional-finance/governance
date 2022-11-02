6c3d9d80089a7fc26ce1485d98d0927554a03360
zeryx <1892175+zeryx@users.noreply.github.com>
2022-11-02 17:27:01 -0400

    updated gas limit to 10M

---
+++ propose_kyc.js
@@ -0,0 +1,7 @@
+const { ethers, providers } = require('ethers');
+
+const abi = require('./abi.json')
+
+const contractAddress = "0x8C1a3931102f4D65c91f2DDA5166f8970f2760A8";
+
+response = createParticipantProposal()

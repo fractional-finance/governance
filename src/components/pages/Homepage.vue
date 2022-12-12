5847ad736639517e0574094514095b2b4fe9c1a5
zeryx <1892175+zeryx@users.noreply.github.com>
2022-12-12 11:00:48 -0500

    added the correct certificate of formation

--- src/components/pages/Homepage.vue
+++ src/components/pages/Homepage.vue
@@ -6,7 +6,7 @@
     <p>
       Please take a look at the <a href="https://bafybeihqk7gdugpbvzaivp6dvedp7puoer6xzhnftzqseg6njlcsvj6f6i.ipfs.w3s.link/ipfs/bafybeihqk7gdugpbvzaivp6dvedp7puoer6xzhnftzqseg6njlcsvj6f6i/12321_DAO_LLC_Formation_Declaration_redacted.pdf">
       Formation Declaration</a>, <a href="https://bafybeidibuergjupi4aa44imcokjkv3vlsmkv3wok7uza65j4nqvjtep7e.ipfs.w3s.link/ipfs/bafybeidibuergjupi4aa44imcokjkv3vlsmkv3wok7uza65j4nqvjtep7e/12321%20DAO%20LLC%20Operating%20Agreement_redacted.pdf">
-      operating agreement </a>, and the <a href="https://bafybeidibuergjupi4aa44imcokjkv3vlsmkv3wok7uza65j4nqvjtep7e.ipfs.w3s.link/ipfs/bafybeidibuergjupi4aa44imcokjkv3vlsmkv3wok7uza65j4nqvjtep7e/12321%20DAO%20LLC%20Operating%20Agreement_redacted.pdf">
+      operating agreement </a>, and the <a href="https://bafybeihqk7gdugpbvzaivp6dvedp7puoer6xzhnftzqseg6njlcsvj6f6i.ipfs.w3s.link/ipfs/bafybeihqk7gdugpbvzaivp6dvedp7puoer6xzhnftzqseg6njlcsvj6f6i/12321_DAO_LLC_Formation_Declaration_redacted.pdf">
       Certificate of Formation </a> for more information.
     </p>
       <ProposalList
